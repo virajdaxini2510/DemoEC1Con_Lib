f6b46eabfd97a894b15ff5851bff9aab934ce6c6
virajdaxini2510 <daxini.viraj@gmail.com>
2026-07-08 07:49:44 +0530

    First Commit 7:48

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEC1Con_Lib
+{
+    class Student
+    {
+        // Data Members
+        public int RollNo;
+        public string Name;
+        public string Course;
+
+        // Method 1 - Accept Student Details
+        public void GetStudentData()
+        {
+            Console.Write("Enter Roll Number : ");
+            RollNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Student Name : ");
+            Name = Console.ReadLine();
+
+            Console.Write("Enter Course : ");
+            Course = Console.ReadLine();
+        }
+
+        // Method 2 - Display Student Details
+        public void DisplayStudentData()
+        {
+            Console.WriteLine("\n===== Student Details =====");
+            Console.WriteLine("Roll Number : " + RollNo);
+            Console.WriteLine("Student Name: " + Name);
+            Console.WriteLine("Course      : " + Course);
+        }
+
+        // Main Method
+        static void Main()
+        {
+            Student s = new Student();
+
+            s.GetStudentData();
+            s.DisplayStudentData();
+
+            Console.ReadKey();
+        }
+    }
+}
